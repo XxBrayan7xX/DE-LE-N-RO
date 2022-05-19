69ca949a646490f1266347e05f7cc22b584b135f
BRAYAN ASAHEL DE LEON ROMERO <bdeleonr7716@tam.conalep.edu.mx>
2022-05-19 14:06:32 -0500

    Se agrego carpeta  PracticaFetch

---
+++ Parcial3/PracticaFetch/ComponentesFetch.php
@@ -0,0 +1,13 @@
+<?php
+$registro['VINumber']  ='I123PO91TS1907';
+$registro['Marca'] ='FORD';
+$registro['Modelo']       ='Mustang';
+$registro['Motor'] ='5.0L (Coyote)';
+$registro['Fabricante'] ='FORD';
+$registro['Pais']     ='USA';
+$registro['Color']     ='Red';
+$registro['Placas']       ='XML86';
+
+$registroJson = json_encode($registro);
+echo $registroJson;
+?>
